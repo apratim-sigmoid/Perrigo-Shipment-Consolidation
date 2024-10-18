915a52692aae176e7e232159febcb4940f5b307c
apratim-sigmoid <apratim.sahu@sigmoidanalytics.com>
2024-10-18 16:37:07 +0530

    Update Consolidation_Streamlit.py

--- Consolidation_Streamlit.py
+++ Consolidation_Streamlit.py
@@ -1154,8 +1154,8 @@ with tab2:
                         position: relative;
                         overflow: hidden;
                     ">
-                        <p style="font-size: 16px; font-weight: 600; margin: 0 0 0px 0; color: #1f77b4; position: relative; z-index: 1;">{title}</p>
-                        <p style="font-size: 18px; font-weight: 600; margin: 0; color: #1f77b4; position: relative; z-index: 1;">{value}</p>
+                        <p style="font-size: 16px; font-weight: 600; margin: 0 0 0px 0; color: #0053a4; position: relative; z-index: 1;">{title}</p>
+                        <p style="font-size: 18px; font-weight: 600; margin: 0; color: #0053a4; position: relative; z-index: 1;">{value}</p>
                         <div style="
                             position: absolute;
                             top: 0;
